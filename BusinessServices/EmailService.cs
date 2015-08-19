4ff3bc1ee2118473fc4af10de3156ea5335b63c1
mhhussain <mo051191@yahoo.com>
2015-08-19 19:47:18 -0400

    Completed email sending code, still need to create new email account with hard coded password.

--- BusinessServices/EmailService.cs
+++ BusinessServices/EmailService.cs
@@ -1,4 +1,7 @@
-﻿using BusinessServices.Contract;
+﻿using System;
+using System.Net;
+using System.Net.Mail;
+using BusinessServices.Contract;
 
 namespace BusinessServices
 {
@@ -6,11 +9,37 @@ namespace BusinessServices
     {
         public bool SendEmail(string to, string from, string subject, string body)
         {
+            var message = new MailMessage();
+
+            // Who we are sending to
+            message.To.Add(@"mhussain@xby2.com");
+
+            message.From = new MailAddress(@"<USERNAME>@xby2.com");
 
 
+            message.Subject = @"You're not actually that cool.";
+            message.Body = @"Click this: http://bit.ly/IqT6zt.";
 
 
-            return false;
+            // Need credentials to send from xby2 domain
+            // TODO: Setup a new email account for "contest@xby2.com" with password "contest_password"
+            var smtp = new SmtpClient(@"mail.xby2.com");
+            smtp.Credentials = new NetworkCredential("contest@xby2.com", "contest_password", "mail.xby2.com");
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = true;
+
+
+            // Attempt to send
+            try
+            {
+                smtp.Send(message);
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
